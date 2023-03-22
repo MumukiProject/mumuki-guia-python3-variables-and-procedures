1e11a70badd9ef2a7d6fc086d2e074934e9016a8
franco@mumuki.org <franco@mumuki.org>
2023-03-22 19:45:08 +0000

    Mumuki Export on 2023-03-22 19:45:08 +0000

--- 00001_Circle of life/test.py
+++ 00001_Circle of life/test.py
@@ -1,18 +1,18 @@
   
   def test_perimeter_of_a_circle_of_radius_1_is_correct(self):
-    self.assertEqual(perimeter_circle(1), 6.28318530717958)
+    self.assertEqual(circle_perimter(1), 6.28318530717958)
   
   def test_perimeter_of_a_circle_of_radius_2_is_correct(self):
-    self.assertEqual(perimeter_circle(2), 12.56637061435916)
+    self.assertEqual(circle_perimter(2), 12.56637061435916)
   
   def test_perimeter_of_a_circle_of_radius_0_is_correct(self):
-    self.assertEqual(perimeter_circle(0), 0)
+    self.assertEqual(circle_perimter(0), 0)
   
   def test_area_of_a_circle_of_radius_1_is_correct(self):
-    self.assertEqual(area_circle(1), 3.14159265358979)
+    self.assertEqual(circle_area(1), 3.14159265358979)
   
   def test_area_of_a_circle_of_radius_2_is_correct(self):
-    self.assertEqual(area_circle(2), 12.56637061435916)
+    self.assertEqual(circle_area(2), 12.56637061435916)
   
   def test_area_of_a_circle_of_radius_0_is_correct(self):
-    self.assertEqual(area_circle(0), 0)
+    self.assertEqual(circle_area(0), 0)
