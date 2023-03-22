3d3a0a535250f1c68c1f8257777e406441d2102a
franco@mumuki.org <franco@mumuki.org>
2023-03-22 20:46:54 +0000

    Mumuki Export on 2023-03-22 20:46:54 +0000

--- 00011_Refill/test.py
+++ 00011_Refill/test.py
@@ -1,18 +1,18 @@
 
-  def test_cargar_500_en_termo_con_200_aumenta_en_500_el_agua_del_termo_y_queda_en_700(self):
-    global agua_del_termo
-    agua_del_termo = 200
-    cargar_termo(500)
-    self.assertEqual(agua_del_termo, 700)
+  def test_refill_500_in_thermos_with_200_increases_in_500_water_in_thermos_and_it_becomes_700(self):
+    global water_in_thermos
+    water_in_thermos = 200
+    refill_thermos(500)
+    self.assertEqual(water_in_thermos, 700)
 
-  def test_cargar_500_en_termo_con_100_aumenta_en_500_el_agua_del_termo_y_queda_en_600(self):
-    global agua_del_termo
-    agua_del_termo = 100
-    cargar_termo(500)
-    self.assertEqual(agua_del_termo, 600)
+  def test_refill_500_in_thermos_with_100_increases_in_500_water_in_thermos_and_it_becomes_600(self):
+    global water_in_thermos
+    water_in_thermos = 100
+    refill_thermos(500)
+    self.assertEqual(water_in_thermos, 600)
 
-  def test_cargar_300_en_termo_con_250_aumenta_en_250_el_agua_del_termo_y_queda_en_550(self):
-    global agua_del_termo
-    agua_del_termo = 300
-    cargar_termo(250)
-    self.assertEqual(agua_del_termo, 550)
+  def test_refill_300_in_thermos_with_250_increases_in_250_water_in_thermos_and_it_becomes_550(self):
+    global water_in_thermos
+    water_in_thermos = 300
+    refill_thermos(250)
+    self.assertEqual(water_in_thermos, 550)
