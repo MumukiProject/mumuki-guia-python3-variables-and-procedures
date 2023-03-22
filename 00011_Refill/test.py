fb0fb8401e93b122fff7cea9280cc6c96be99ec3
franco@mumuki.org <franco@mumuki.org>
2023-03-22 20:48:05 +0000

    Mumuki Export on 2023-03-22 20:48:05 +0000

--- 00011_Refill/test.py
+++ 00011_Refill/test.py
@@ -1,17 +1,17 @@
 
-  def test_refill_500_in_thermos_with_200_increases_in_500_water_in_thermos_and_it_becomes_700(self):
+  def test_refill_500_ml_in_thermos_with_200_ml_increases_in_500_ml_water_in_thermos_and_it_becomes_700_ml(self):
     global water_in_thermos
     water_in_thermos = 200
     refill_thermos(500)
     self.assertEqual(water_in_thermos, 700)
 
-  def test_refill_500_in_thermos_with_100_increases_in_500_water_in_thermos_and_it_becomes_600(self):
+  def test_refill_500_ml_in_thermos_with_100_ml_increases_in_500_ml_water_in_thermos_and_it_becomes_600_ml(self):
     global water_in_thermos
     water_in_thermos = 100
     refill_thermos(500)
     self.assertEqual(water_in_thermos, 600)
 
-  def test_refill_300_in_thermos_with_250_increases_in_250_water_in_thermos_and_it_becomes_550(self):
+  def test_refill_300_ml_in_thermos_with_250_ml_increases_in_250_ml_water_in_thermos_and_it_becomes_550_ml(self):
     global water_in_thermos
     water_in_thermos = 300
     refill_thermos(250)
