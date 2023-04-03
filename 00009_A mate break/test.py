2a9f24d0892bc5045451ec852cd75e31fbbd781a
franco@mumuki.org <franco@mumuki.org>
2023-04-03 19:53:44 +0000

    Mumuki Export on 2023-04-03 19:53:44 +0000

--- 00009_A mate break/test.py
+++ 00009_A mate break/test.py
@@ -2,13 +2,13 @@
   def test_water_in_thermos_is_initially_at_1000(self):
     self.assertEqual(water_in_thermos, 1000, "water_in_thermos must be initialized with 1000")
 
-  def test_pour_water_decreases_30_ml_water_in_thermos(self):
+  def test_when_pouring_water_water_in_thermos_decreases_30_ml(self):
     global water_in_thermos
     water_in_thermos = 1000
     pour_water()
     self.assertEqual(water_in_thermos, 970)
 
-  def test_pouring_3_mates_decreases_90_ml_water_in_thermos(self):
+  def test_when_pouring_3_mates_water_in_thermos_decreases_90_ml(self):
     global water_in_thermos
     water_in_thermos = 1000
     pour_water()
